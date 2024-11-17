e9b5af4339263b46c9d5c39a4674aaf665a1735b
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2024-11-17 19:06:42 +0500

    ok 17112024 1906

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category
+from .models import Topping
+from .models import Wrapper
+from .models import IceCream
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.register(IceCream)
