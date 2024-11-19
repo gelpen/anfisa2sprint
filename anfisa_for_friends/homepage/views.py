237cbf63f396f693a57bff0433e74e21315575ba
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2024-11-19 23:30:30 +0500

    ok 19112024 2330

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -5,11 +5,14 @@ from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    # Запрос:
-    ice_cream_list = IceCream.objects.all()
-    # Полученный из БД QuerySet передаём в словарь контекста:
+    # Возьмём нужное. А ненужное не возьмём:
+    ice_cream_list = IceCream.objects.values(
+            'id', 'title', 'description'
+        # Верни только те объекты, у которых в поле is_on_main указано True:
+        #).filter(is_on_main=True)
+        # Исключи те объекты, у которых is_published=False:
+        ).exclude(is_published=False)
     context = {
         'ice_cream_list': ice_cream_list,
     }
-    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
     return render(request, template_name, context)
