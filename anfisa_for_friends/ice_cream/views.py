fe9facbde2a9dedc4754bd9137b78d12447a7e8a
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2024-11-20 21:24:08 +0500

    ok 20112024 2124

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from ice_cream.models import IceCream
-
+from django.shortcuts import get_object_or_404, render
 
 # def ice_cream_detail(request, pk):
 #     template = 'ice_cream/detail.html'
@@ -10,7 +10,13 @@ from ice_cream.models import IceCream
 def ice_cream_detail(request, pk):
     template_name = 'ice_cream/detail.html'
     # Вызываем .get() и в его параметрах указываем условия фильтрации:
-    ice_cream = IceCream.objects.get(pk=pk)
+    # ice_cream = IceCream.objects.get(pk=pk)
+    # ice_cream = get_object_or_404(IceCream, pk=pk)
+    ice_cream = get_object_or_404(
+        # Первый аргумент - QuerySet:
+        IceCream.objects.values('title', 'description').filter(is_published=True),
+        # Второй аргумент - условие, по которому фильтруются записи из QuerySet:
+        pk=pk)
     context = {
         'ice_cream': ice_cream,
     }
