cc58dcd561df50b37acba3572597e0b28b58a7df
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2024-11-21 18:59:03 +0500

    ok 21112024 1859

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -9,14 +9,10 @@ from django.shortcuts import get_object_or_404, render
 
 def ice_cream_detail(request, pk):
     template_name = 'ice_cream/detail.html'
-    # Вызываем .get() и в его параметрах указываем условия фильтрации:
-    # ice_cream = IceCream.objects.get(pk=pk)
-    # ice_cream = get_object_or_404(IceCream, pk=pk)
     ice_cream = get_object_or_404(
-        # Первый аргумент - QuerySet:
-        IceCream.objects.values('title', 'description').filter(is_published=True),
-        # Второй аргумент - условие, по которому фильтруются записи из QuerySet:
-        pk=pk)
+        IceCream.objects.filter(is_published=True, category__is_published=True),
+        pk=pk
+    )
     context = {
         'ice_cream': ice_cream,
     }
@@ -25,5 +21,10 @@ def ice_cream_detail(request, pk):
 
 def ice_cream_list(request):
     template = 'ice_cream/list.html'
-    context = {}
-    return render(request, template, context)
+    ice_cream_list = IceCream.objects.select_related('category').filter(
+        is_published=True,
+        category__is_published=True
+    ).order_by('category')
+
+    context = {'ice_cream_list': ice_cream_list}
+    return render(request, template, context) 
